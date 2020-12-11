ea62099acd51a96e2d572c36a5e34878d80963d3
Hugo Figueroa <hugofiguer97@gmail.com>
2020-12-11 00:20:34 -0600

    Commit inicial

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+// const mysql = require('./app/model/db');
+const app = express();
+const bodyParser = require('body-parser');
+
+var cors = require('cors');
+
+port = '3000';
+
+app.listen(port, function() {
+  console.log('Servidor escuchando en el puerto 3000');
+});
+
+app.use(cors());
+app.use(bodyParser.json({ limit: '5mb', extended: true }));
+
+// var routes = require('./app/routes/appRoutes');
+// routes(app);
